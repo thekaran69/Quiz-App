9c9dbbb76f7fac06d5f5890b6f9a15d8d441bbfd
Karan Singh <119948976+thekaran69@users.noreply.github.com>
2024-05-28 21:52:36 +0530

    final updated

--- app.js
+++ app.js
@@ -1,36 +1,97 @@
-const questions = [{
-    
-    'que': 'Which of the following is a markeup language?',
-    'a' : 'HTML',
-    'b' : 'CSS',
-    'c' : 'PHP',
-    'd' : 'JAVASCRIPT',
-    'correct': 'a'
-},
-{
-    'que': 'What is the term for a section of code that can be reused throughout a program?',
-    'a': 'Loop',
-    'b': 'Variable',
-    'c': 'Function',
-    'd': 'Comment',
-    'correct': 'c'
-},
-{
-    'que': 'In web development, what does URL stand for?',
-    'a': 'Universal Resource Link',
-    'b': 'Uniform Resource Locator',
-    'c': 'Unified Routing Link',
-    'd': 'User Requested Location',
-    'correct': 'b'
-    
-}]
 
-let index = 1;
-const quesBox  = document.getElementById("quesBox");
-const loadQuestion = () =>  {
-    const data = questions[index]
-    console.log(data);
-    quesBox.innerText = `${index+1}) ${data.que}`;
+const quizData = [{
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
 }
 
-loadQuestion();
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
